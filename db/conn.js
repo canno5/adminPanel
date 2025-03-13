d68ca203f068531a2859fe79c1dc7e5355a62a60
canno5 <canandeveloper@gmail.com>
2025-03-13 14:32:21 +0500

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,13 +1,6 @@
 const mogoose = require("mongoose");
 const URI  = process.env.MONGODB_URI
 
-
-
-// mogoose.connect(URI).then(()=>{
-//     console.log("Connection is Sucess")
-// }).catch((err)=>{
-//     console.log("Connection is Falied "+ err);
-// });
 const connectDB = async () => {
     try {
         await mogoose.connect(URI);
